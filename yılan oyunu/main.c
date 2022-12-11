d85a2735768668f242d6179d65f7ce1ea2061163
DEFNE <defneew@outlook.com>
2022-12-11 22:11:43 +0300

    yilan oyunu
    c programi ile basit seviyede yilan oyunu yaptim.

---
+++ yılan oyunu/main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+int saha[80][23]={0};
+int k,l;
+int kuyrukSutun[300];
+int kuyrukSatir[300];
+void gotoxy(short x, short y){
+  HANDLE hConsoleOutput;
+  COORD Cursor_Pos={x-1,y-1};
+
+  hConsoleOutput= GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleCursorPosition(hConsoleOutput, Cursor_Pos);
+}
+void cerceve(int x1,int y1,int x2,int y2,int tip){
+  int i;
+  for(i=x1;i<=x2;i++)
+  {
+      saha[i][y1]=tip;
+      saha[i][y2]=tip;
+  }
+  for(i=y1;i<=y2;i++)
+  {
+      saha[x1][i]=tip;
+      saha[x2][i]=tip;
+  }
+}
+void ekranYaz()
+{
+    int x,y;
+    for(x=0;x<80;x++){
+        for(y=0;y<23;y++){
+            gotoxy(x+1,y+1);
+            if(saha[x][y]==2){
+                printf("%c",176);
+            }
+        }
+    }
+}
+int randomAta()
+{
+    srand(time(NULL));
+    k=4+rand()%65;
+    l=4+rand()%15;
+
+}
+void main()
+{
+    randomAta();
+    cerceve(0,0,79,22,2);
+    ekranYaz();
+    int sx=40,sy=12;
+    unsigned char tus;
+    int dy=0,dx=0; int a=0; int devam=1;;
+    do
+    {
+        if(kbhit()){
+            tus=getch();
+            if(tus==224){
+                tus=getch();
+                switch(tus)
+                {
+                    case 72: dy=-1; dx=0;
+                    break;
+                    case 80: dy=1; dx=0;
+                    break;
+                    case 77: dx=1; dy=0;
+                    break;
+                    case 75: dx=-1; dy=0;
+                    break;
+                }
+            }
+        }
+        sx=sx+dx;
+        sy=sy+dy;
+        if(sx>78)
+            sx=2;
+        if(sx<2)
+            sx=78;
+        if(sy>22)
+            sy=2;
+        if(sy<2)
+            sy=22;
+            kuyrukSutun[0]=sx;
+            kuyrukSatir[0]=sy;
+            for(int i=1;i<=a;i++)
+            {
+                if(sx==kuyrukSutun[i]&& sy==kuyrukSatir[i]){
+                    gotoxy(sx,sy); printf("---YANDINIZ---");
+                    getch;
+                    devam=0;
+                }
+            }
+            if(sx==k && sy==l)
+            {
+                randomAta();
+                a++;
+                gotoxy(2,2); printf("Puan: %d",a);
+            }
+            gotoxy(k,l); printf("A");
+
+        gotoxy(sx,sy); printf("*");
+        for( int i=0;i<=a;i++){
+            gotoxy(kuyrukSutun[i],kuyrukSatir[i]); printf("*");
+        }
+        Sleep(60);
+        gotoxy(sx,sy); printf(" ");
+        for( int i=0;i<=a;i++){
+            gotoxy(kuyrukSutun[i],kuyrukSatir[i]); printf(" ");
+        }
+        for(int i=a;i>0;i--){
+            kuyrukSutun[i]=kuyrukSutun[i-1];
+            kuyrukSatir[i]=kuyrukSatir[i-1];
+        }
+    }while(devam==1);
+
+}
